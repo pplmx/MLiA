be6c3b6bb9ab08ccb578c70598414be4c3efa853
mystic-cg <215104920@qq.com>
2018-06-14 20:36:17 +0800

    [test] model evaluation and choice

---
+++ chapter02/model_evaluation&choice.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author  : mystic
+# @date    : 2018/6/14 20:23
+"""
+    错误率:
+        通常我们把分类错误的样本数占样本总数的比例称为"错误率(error rate)"
+        即如果在m个样本中有a个样本分类错误,则错误率 E = a/m
+    精度:
+        相应的, 1-a/m称为"精度(accuracy)"
+        即"精度=1-错误率"
+    误差:
+        我们把学习期的实际预测输出和样本的真实输出之间的差异称为"误差(error)"
+        学习器在训练集上的误差,称为"训练误差(training error)或经验误差(empirical error)"
+            在新样本上的误差,称为"泛化误差(generalization error)"
+    过拟合(overfitting):
+    欠拟合(underfitting):
+"""
