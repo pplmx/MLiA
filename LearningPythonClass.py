4151773096569bbd1f6d784279c4de08465666b6
mystic <215104920@qq.com>
2018-12-07 21:15:30 +0800

    [learning] python class

--- LearningPythonClass.py
+++ LearningPythonClass.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 # @author  : mystic
 # @date    : 2018/12/6 20:54
+import logging
 from enum import Enum, unique
+logging.basicConfig(level=logging.INFO)
 
 
 class Student(object):
@@ -17,8 +19,17 @@ class Student(object):
     @name.setter
     def name(self, name):
         if not isinstance(name, str):
-            raise TypeError('Expected a string')
+            raise TypeError("Expected a string")
         self.__name = name
+
+    @staticmethod
+    def divide(x, y):
+        try:
+            return lambda: x / y
+        except ZeroDivisionError as e:
+            logging.error(e)
+        finally:
+            logging.info("End to divide.")
 
 
 @unique
@@ -30,4 +41,3 @@ class Weekday(Enum):
     THU = 4
     FRI = 5
     Sat = 6
-
