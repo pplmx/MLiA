5106284857b688d07d077c9c7620a334ed7ce88d
mystic <215104920@qq.com>
2018-12-08 20:48:36 +0800

    [learning] python process

--- LearningPythonClass.py
+++ LearningPythonClass.py
@@ -3,7 +3,12 @@
 # @author  : mystic
 # @date    : 2018/12/6 20:54
 import logging
+import os
+import random
+import time
 from enum import Enum, unique
+from multiprocessing import Process, Pool
+
 logging.basicConfig(level=logging.INFO)
 
 
@@ -41,3 +46,35 @@ class Weekday(Enum):
     THU = 4
     FRI = 5
     Sat = 6
+
+
+def run(name):
+    print('Run child process %s (%s)' % (name, os.getpid()))
+
+
+def long_time_task(name):
+    print('Run task %s (%s)' % (name, os.getpid()))
+    start = time.time()
+    time.sleep(random.random() * 3)
+    end = time.time()
+    print('Task %s runs %0.2f seconds.' % (name, (end - start)))
+
+
+if __name__ == "__main__":
+    print('Parent process %s.' % os.getpid())
+    # create a child process
+    p = Process(target=run, args=('test',))
+    print('Child process will start.')
+    # start child process
+    p.start()
+    p.join()
+    print('Child process end.')
+    print("==========================================")
+    # default value is your CPU cores, mine is 4(Pool()<=>Pool(4)[On My Computer])
+    p = Pool(5)
+    for i in range(5):
+        p.apply_async(long_time_task, args=(i,))
+    print('Waiting for all sub-processes done...')
+    p.close()
+    p.join()
+    print('All sub-processes done.')
