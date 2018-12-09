035de088e1faf16caab0a4cdc2538c6d4dce5c67
mystic <215104920@qq.com>
2018-12-09 20:31:45 +0800

    [learning] python process

--- LearningPythonClass.py
+++ LearningPythonClass.py
@@ -5,9 +5,10 @@
 import logging
 import os
 import random
+import subprocess
 import time
 from enum import Enum, unique
-from multiprocessing import Process, Pool
+from multiprocessing import Process, Pool, Queue
 
 logging.basicConfig(level=logging.INFO)
 
@@ -60,6 +61,21 @@ def long_time_task(name):
     print('Task %s runs %0.2f seconds.' % (name, (end - start)))
 
 
+def write(queue: Queue):
+    print('Process to write: %s' % os.getpid())
+    for val in ['A', 'B', 'C']:
+        print('Put %s to queue...' % val)
+        queue.put(val)
+        time.sleep(random.random)
+
+
+def read(queue: Queue):
+    print('Process to read: %s' % os.getpid())
+    while True:
+        val = queue.get(True)
+        print('Get %s from queue.' % val)
+
+
 if __name__ == "__main__":
     print('Parent process %s.' % os.getpid())
     # create a child process
@@ -78,3 +94,20 @@ if __name__ == "__main__":
     p.close()
     p.join()
     print('All sub-processes done.')
+    print('==========================================')
+    print('nslookup baidu.com')
+    r = subprocess.call(['nslookup', 'baidu.com'])
+    print('Exit code %s' % r)
+    print('==========================================')
+    # create Queue
+    queue_ = Queue()
+    proc_w = Process(target=write, args=(queue_,))
+    proc_r = Process(target=read, args=(queue_,))
+    # start process to write
+    proc_w.start()
+    # start process to read
+    proc_r.start()
+    # wait for ending to proc_w
+    proc_w.join()
+    # proc_r is infinity loop, forced stop
+    proc_r.terminate()
