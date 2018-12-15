8cc85d147ea4236b2e7dad76f67f5f72cfd6ec00
mystic <215104920@qq.com>
2018-12-15 21:30:36 +0800

    [learning] Coroutine

--- coroutine.py
+++ coroutine.py
@@ -2,4 +2,16 @@
 # -*- coding: utf-8 -*-
 # @author  : mystic
 # @date    : 12/14/2018 21:18
+import asyncio
 
+
+@asyncio.coroutine
+def hello():
+    print('Hello World!')
+    yield from asyncio.sleep(1)
+    print('Hello again!')
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(hello())
+loop.close()
